ec575d8a5bb2c2e38b1d55c23a54813631580548
Tanguy <tanguy.freycon@gmail.com>
2023-03-17 08:51:48 +0100

    Initial npm and webpack configuration

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import _ from 'lodash';
+const path = require('path-browserify');
+import './style.css';
+
+class Component {
+    constructor(innerHTML) {
+        const element = document.createElement('div');
+        element.innerHTML = innerHTML;
+
+        return element;
+    }
+}
+
+document.body.appendChild(new Component(
+    _.join(['Hello', 'webpack'], ' ')
+));
+
+document.body.appendChild(new Component(
+    window.location
+));
+
+document.body.appendChild(new Component(
+    'log'
+));
+
+document.body.appendChild(new Component(
+    "J'ai preseque fini pour ce soir et aujourd'hui. Je n'ai peut être pas fait grand chose, mais comme souvent cela m'a permit d'ouvrir les champs des possibles!"
+));
+
+
